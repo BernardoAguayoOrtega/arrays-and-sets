3454a74755b611b9992bbb86e465d807f82b5fe6
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-20 09:02:56 -0500

    MOre lfexible arrays with destructuring

---
+++ src/07.js
@@ -0,0 +1,14 @@
+const finalMenuItems = [
+  "American Cheeseburger",
+  "Southern Fried Chicken",
+  "Glazed Salmon"
+];
+
+const [first, second] = finalMenuItems;
+
+// let first = finalMenuItems[0];
+// let second = finalMenuItems[1];
+// let third = finalMenuItems[2];
+
+// console.log(first, second, third);
+console.log({ first },{ second });
