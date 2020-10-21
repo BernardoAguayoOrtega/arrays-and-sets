683c1bdc81cc9d73e7a4519511e60a65e137b748
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-21 11:33:11 -0500

    Update files

--- src/07.js
+++ src/07.js
@@ -13,3 +13,24 @@ const [second_, first_] = [first, second];
 
 // console.log(first, second, third);
 console.log({ first_ }, { second_ });
+
+//We can use array destructuring to 
+//take values out of an array and store them into variables:
+
+let [first, second] = someArray
+
+//We can also use destructuring to 
+//swap the values between two variables:
+
+const [second, first] = [first, second];
+
+//Array destructuring is very similar to object
+//destructuring, in this case you destructuring by 
+//order of array instead object destructuring by name.
+
+//example:
+const array = [1,2,3]
+const [second, first] = array;
+
+// returns second = 1, first = 2
+//So doesn't matter the name, matters the position
